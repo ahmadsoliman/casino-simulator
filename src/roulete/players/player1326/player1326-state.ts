43301c261ea8d423ce8d02fef2423a0c5614582a
Ahmad Soliman <ahmadadel92@gmail.com>
2023-11-15 20:25:00 +0100

    refactor player1326 state classes to only set attributes and let parent class handle logic, and implement a new singleton factory class to avoid instantiating multiple instances

--- src/roulete/players/player1326/player1326-state.ts
+++ src/roulete/players/player1326/player1326-state.ts
@@ -1,16 +1,30 @@
 import { Bet } from "../../../core";
 import { Player1326 } from "./player1326";
-import { Player1326NoWins } from "./player1326-states";
+import {
+  Player1326NoWins,
+  Player1326States,
+  STATE_FACTORY,
+} from "./player1326-states";
 
 export abstract class Player1326State {
+  // Has to be instantiated by children
+  protected _betMultiplier: number = 1;
+  protected _nextStateWin?: Player1326State;
+
   constructor(protected _player: Player1326) {}
 
   currentBet(): Bet {
-    throw "currentBet() isn't implemented";
+    return new Bet(
+      this._betMultiplier * this._player.table.minimum,
+      this._player.outcome
+    );
   }
 
   nextWon(): Player1326State {
-    throw "nextWon() isn't implemented";
+    return (
+      this._nextStateWin ||
+      STATE_FACTORY.get(Player1326States.NO_WINS, this._player)
+    );
   }
 
   nextLost(): Player1326State {
--- src/roulete/players/player1326/player1326-states.ts
+++ src/roulete/players/player1326/player1326-states.ts
@@ -1,42 +1,79 @@
-import { Bet } from "../../../core";
+import { Player1326 } from "./player1326";
 import { Player1326State } from "./player1326-state";
 
-export class Player1326NoWins extends Player1326State {
-  currentBet(): Bet {
-    return new Bet(this._player.table.minimum, this._player.outcome);
-  }
+export enum Player1326States {
+  NO_WINS,
+  ONE_WIN,
+  TWO_WINS,
+  THREE_WINS,
+}
 
-  nextWon(): Player1326State {
-    return new Player1326OneWin(this._player);
+export class Player1326StateFactory {
+  private _values: { [key: number]: Player1326State } = {};
+
+  get(type: Player1326States, player: Player1326) {
+    if (this._values[type]) {
+      return this._values[type];
+    }
+
+    switch (type) {
+      case Player1326States.ONE_WIN:
+        this._values[type] = new Player1326OneWin(player);
+        break;
+      case Player1326States.TWO_WINS:
+        this._values[type] = new Player1326TwoWins(player);
+        break;
+      case Player1326States.THREE_WINS:
+        this._values[type] = new Player1326ThreeWins(player);
+        break;
+      case Player1326States.NO_WINS:
+      default:
+        this._values[type] = new Player1326NoWins(player);
+    }
+    return this._values[type];
+  }
+}
+// Singleton factory
+export const STATE_FACTORY = new Player1326StateFactory();
+
+export class Player1326NoWins extends Player1326State {
+  constructor(protected _player: Player1326) {
+    super(_player);
+    this._betMultiplier = 1;
+    this._nextStateWin = STATE_FACTORY.get(
+      Player1326States.ONE_WIN,
+      this._player
+    );
   }
 }
 
 export class Player1326OneWin extends Player1326State {
-  currentBet(): Bet {
-    return new Bet(3 * this._player.table.minimum, this._player.outcome);
-  }
-
-  nextWon(): Player1326State {
-    return new Player1326TwoWins(this._player);
+  constructor(protected _player: Player1326) {
+    super(_player);
+    this._betMultiplier = 3;
+    this._nextStateWin = STATE_FACTORY.get(
+      Player1326States.TWO_WINS,
+      this._player
+    );
   }
 }
 
 export class Player1326TwoWins extends Player1326State {
-  currentBet(): Bet {
-    return new Bet(2 * this._player.table.minimum, this._player.outcome);
-  }
-
-  nextWon(): Player1326State {
-    return new Player1326ThreeWins(this._player);
+  constructor(protected _player: Player1326) {
+    super(_player);
+    this._betMultiplier = 2;
+    this._nextStateWin = STATE_FACTORY.get(
+      Player1326States.THREE_WINS,
+      this._player
+    );
   }
 }
 
 export class Player1326ThreeWins extends Player1326State {
-  currentBet(): Bet {
-    return new Bet(6 * this._player.table.minimum, this._player.outcome);
-  }
-
-  nextWon(): Player1326State {
-    return new Player1326NoWins(this._player);
+  constructor(protected _player: Player1326) {
+    super(_player);
+    this._betMultiplier = 6;
+    // handled in parent class to avoid loop instantiation
+    this._nextStateWin = undefined;
   }
 }
